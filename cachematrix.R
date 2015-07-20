71a77a3bcb05e16035a422c0026c6b881c6766e3
lasisi75 <lasisi75@yahoo.com>
2015-07-20 15:34:35 -0600

    Solution to programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## I implement two functions that (1) creates a special "matrix" object which can also catch its inverse. 
+## (2) another function that compute the inverse of a matrix. This functions checks for the existence of the inverse of a matrix, 
+## if one is found, the catched inverse is returned, otherwise, a new inverse is computed
 
-## Write a short comment describing this function
+## the function creates a special "matrix" object which can also catch its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	
+	inverseMatrix <- NULL
+	
+	setMatrix <- function(y) {
+		x <<- y
+		inverseMatrix <<- NULL
+	}
+	
+	getMatrix <- function() {
+		x 
+	}
+	
+	setInverseMatrix <- function(invMatrix) {
+		
+		inverseMatrix <<- invMatrix
+	}
+	
+	getInverseMatrix <- function() {
+		
+		inverseMatrix
+	}
+	
+	list(setMatrix = setMatrix, getMatrix = getMatrix, 
+	     setInverseMatrix = setInverseMatrix, getInverseMatrix = getInverseMatrix)
 }
 
 
-## Write a short comment describing this function
+## this function uses the makeCacheMatrix function to check existence of the inverse of a matrix in the cache or compute a new inverse
+## if not found 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        inverseMatrix <- x$getInverseMatrix()
+        
+        if(!is.null(inverseMatrix)){
+        	   message("getting cached matrix")
+        	   return (inverseMatrix)
+        }
+        
+        originalMatrix <- x$getMatrix()
+        inverseMatrix <- solve(originalMatrix)
+        x$setInverseMatrix(inverseMatrix)
+        inverseMatrix
 }
